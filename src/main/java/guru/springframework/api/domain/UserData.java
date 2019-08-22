c0c2c5bf4c371c0af224c3e14a94ef87c7ec8e2b
Danijel Petanovic <danijel.petanovic@ericsson.com>
2019-08-22 16:31:28 +0200

    Created Data Model

---
+++ src/main/java/guru/springframework/api/domain/UserData.java
@@ -0,0 +1,15 @@
+package guru.springframework.api.domain;
+
+import java.util.List;
+
+public class UserData {
+    List<User> user;
+
+    public List<User> getUser() {
+        return user;
+    }
+
+    public void setUser(List<User> user) {
+        this.user = user;
+    }
+}
